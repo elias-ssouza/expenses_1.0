ed948c039868044a4f395332e6b865a3c53429b8
Elias Souza <elias.ssouza@hotmail.com>
2024-07-26 20:33:05 -0300

    adicionando fontes personalizadas e imagem

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,123 @@
-import 'dart:math';
-
 import 'package:expenses/components/transaction_form.dart';
-import 'package:expenses/components/transaction_list.dart';
-import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
 
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
+  ExpensesApp({Key? key}) : super(key: key);
+  final ThemeData tema = ThemeData();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:MyHomePage(),
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
+      home: const MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
+          secondary: Colors.amber,
         ),
+        textTheme: tema.textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
 
-  class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transactions = [
+    // Transaction(
+    //   id: 't1',
+    //   title: 'Novo Tênis de Corrida',
+    //   value: 310.76,
+    //   date: DateTime.now(),
+    // ),
+    // Transaction(
+    //   id: 't2',
+    //   title: 'Conta de Luz',
+    //   value: 211.30,
+    //   date: DateTime.now(),
+    // ),
+  ];
 
-  final _transactions = [
-      Transaction(
-        id: 't1', title: 'Tênis novo', value: 349.99, date: DateTime.now()),
-      Transaction(
-        id: 't2', title: 'Luz', value: 150.49, date: DateTime.now())
-    ];
-
-    _addTransaction(String title, double value){
+  _addTransaction(String title, double value) {
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(),
-      title: title, 
-      value: value, 
-      date: DateTime.now()
-     );
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
 
-     setState(() {
-       _transactions.add(newTransaction);
-     });
+    setState(() {
+      _transactions.add(newTransaction);
+    });
 
-     Navigator.of(context).pop();
-}
+    Navigator.of(context).pop();
+  }
 
-    _openTransactionFormModal(BuildContext context){
-      showModalBottomSheet(
-        context: context, 
-        builder: (_){
-          return TransactionForm(_addTransaction);
-        }
-      );
-    }
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
+  }
 
-    @override
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar:AppBar(title: Text('Despesas Pessoais'),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
         actions: [
           IconButton(
+            icon: const Icon(Icons.add),
             onPressed: () => _openTransactionFormModal(context),
-            icon: Icon(Icons.add))
+          ),
         ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                child: Card(
-                  color: Colors.blue,
-                  child: Text('Gráfico'),
-                  elevation: 5,
-                ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
               ),
-              TransactionList(_transactions),
-            ],
-          ),
+            ),
+            TransactionList(_transactions),
+          ],
         ),
-        floatingActionButton: FloatingActionButton(
-          child: 
-          Icon(Icons.add),
-          onPressed: () => _openTransactionFormModal(context),
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      );
-    }
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => _openTransactionFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
 }
