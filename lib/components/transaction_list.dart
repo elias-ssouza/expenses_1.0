374dbea67e199180e64f0bdc59fc7f025636cb54
Elias Souza <elias.ssouza@hotmail.com>
2024-07-27 21:57:19 -0300

    finalização do chartbar, criação do método de remoção e seleção de data

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -4,8 +4,9 @@ import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+  final void Function(String) onRemove;
 
-  const TransactionList(this.transactions, {Key? key}) : super(key: key);
+  const TransactionList(this.transactions, this.onRemove);
 
   @override
   Widget build(BuildContext context) {
@@ -34,49 +35,38 @@ class TransactionList extends StatelessWidget {
               itemBuilder: (ctx, index) {
                 final tr = transactions[index];
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).colorScheme.primary,
-                            width: 2,
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.purple,
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child: Text('${tr.value.toStringAsFixed(2)}',
+                          style: TextStyle(color: Colors.grey[100]),
                           ),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Theme.of(context).colorScheme.primary,
                           ),
-                        ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                          Text(
-                            DateFormat('d MMM y').format(tr.date),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
+                    ),
+                    title: Text(tr.title,
+                    style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(
+                      DateFormat('d MMM y').format(tr.date),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () => onRemove(tr.id), 
+                      icon: const Icon(Icons.delete),
+                      color: Theme.of(context).colorScheme.error,
                       ),
-                    ],
                   ),
                 );
-              },
-            ),
+  }),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -28,13 +28,13 @@ class ExpensesApp extends StatelessWidget {
             color: Colors.black,
           ),
         ),
-//        appBarTheme: const AppBarTheme(
-//          titleTextStyle: TextStyle(
-//            fontFamily: 'OpenSans',
-//            fontSize: 20,
-//            fontWeight: FontWeight.bold,
-//          ),
-//        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
@@ -48,20 +48,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _transactions = [
-     Transaction(
-       id: 't1',
-       title: 'Novo Tênis de Corrida',
-       value: 310.76,
-       date: DateTime.now().subtract(Duration(days: 3)),
-     ),
-     Transaction(
-       id: 't2',
-       title: 'Conta de Luz',
-       value: 211.30,
-       date: DateTime.now().subtract(Duration(days: 4)),
-     ),
-  ];
+  final List<Transaction> _transactions = [];
 
   List<Transaction> get _recentTransactions{
     return _transactions.where((tr) {
@@ -69,12 +56,12 @@ class _MyHomePageState extends State<MyHomePage> {
     }).toList();
   }
 
-  _addTransaction(String title, double value) {
+  _addTransaction(String title, double value, DateTime date) {
     final newTransaction = Transaction(
       id: Random().nextDouble().toString(),
       title: title,
       value: value,
-      date: DateTime.now(),
+      date: date,
     );
 
     setState(() {
@@ -82,6 +69,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
 
     Navigator.of(context).pop();
+  }
+
+  _removeTransaction(String id){
+    setState(() {
+      _transactions.removeWhere((tr) => tr.id == id);
+    });
   }
 
   _openTransactionFormModal(BuildContext context) {
@@ -110,12 +103,14 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Chart(_recentTransactions),
-            TransactionList(_transactions),
+            TransactionList(_transactions, _removeTransaction),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
+        backgroundColor: Colors.purple,
+        child: const Icon(Icons.add,
+        color: Color.fromARGB(255, 225, 224, 224),),
         onPressed: () => _openTransactionFormModal(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
