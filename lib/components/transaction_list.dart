d33391ac8f8bff0f226677bf4a74d53fdfdfc40e
Elias Souza <elias.ssouza@hotmail.com>
2024-07-30 23:56:47 -0300

    tornando a transaction list responsiva

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,22 +11,29 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
+        ? LayoutBuilder(
+          builder: (ctx, constraints){
+            return Column(
             children: [
-              const SizedBox(height: 20),
-              Text(
-                'Nenhuma Transação Cadastrada!',
-                style: Theme.of(context).textTheme.titleLarge,
+              SizedBox(height: constraints.maxHeight * 0.05),
+              Container(
+                height: constraints.maxHeight * 0.3,
+                child: Text(
+                  'Nenhuma Transação Cadastrada!',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
               ),
-              const SizedBox(height: 20),
+              SizedBox(height: constraints.maxHeight * 0.05),
               SizedBox(
-                height: 200,
+                height: constraints.maxHeight * 0.6,
                 child: Image.asset(
                   'assets/images/waiting.png',
                   fit: BoxFit.cover,
                 ),
               ),
             ],
+          );
+          }
           )
         : ListView.builder(
             itemCount: transactions.length,
